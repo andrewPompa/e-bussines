6d9cd7989baa21389eada7c505bc7d0db243ad43
mijo <mijo@ailleron.com>
2018-05-07 21:55:53 +0200

    [DB] Initial db schema

--- shop-project/conf/evolutions/default/1.sql
+++ shop-project/conf/evolutions/default/1.sql
@@ -1,16 +1,43 @@
 # --- !Ups
-
-create table "category" (
-  "id" integer not null primary key autoincrement,
-  "name" varchar not null
-);
-
 create table "product" (
   "id" integer not null primary key autoincrement,
   "name" varchar not null,
+  "price" numeric not null,
   "description" text not null,
   category int not null,
   foreign key(category) references category(id)
+);
+
+create table "category" (
+"id" integer not null primary key autoincrement,
+"name" varchar not null
+);
+
+
+create table "product_opinion" (
+  "product_id" integer not null,
+  "opinion_id" integer not null,
+  foreign key (product_id) references product (id),
+  foreign key (opinion_id) references opinion (id)
+);
+
+
+create table "opinion" (
+  "id" integer not null primary key autoincrement,
+  "text" text not null
+);
+
+create table "order" (
+  "id"         integer not null primary key autoincrement,
+  "product_id" integer not null,
+  foreign key (product_id) references product (id)
+);
+
+create table "pricing" (
+  "id"         integer not null primary key autoincrement,
+  "order_id" integer not null,
+  "done" boolean default false,
+  foreign key (order_id) references order (id)
 );
 
 
@@ -18,3 +45,7 @@ create table "product" (
 
 drop table "product" if exists;
 drop table "category" if exists;
+drop table "product_opinion" if exists;
+drop table "opinion" if exists;
+drop table "order" if exists;
+drop table "pricing" if exists;
